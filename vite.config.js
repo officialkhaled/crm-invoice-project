88a346519db4ed5f3140537dac01a3f671036e77
Khaled <khaled@skylarksoft.com>
2025-02-23 15:24:51 +0600

    ngrok setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        cors: {
+            origin: '*',
+        },
+        hmr: {
+            protocol: 'wss',
+            host: '0d1d-103-137-229-152.ngrok-free.app',
+        },
+    },
     plugins: [
         laravel({
             input: [
